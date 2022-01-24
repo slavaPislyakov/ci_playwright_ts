9f08bbb3f9fd7fa5728b2b13b6d66447aa60e93f
vyacheslav_pislyakov <badik216@gmail.com>
2022-01-24 08:43:25 +0300

    add launchOptions for wdio config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,27 @@ const config: PlaywrightTestConfig = {
     headless: false,
     video: "retain-on-failure",
     screenshot: "only-on-failure",
+    launchOptions: {
+      args: [
+        "--whitelisted-ips=",
+        "--disable-infobars=true", // note this does not remove "Chrome is being controlled by automated test software" notification
+        "--disable-gpu",
+        "window-size=1920,1080",
+        "test-type=browser",
+        "disable-notifications",
+        "incognito",
+        "disable-application-cache",
+        "-disable-extensions", // used to bypass loading of extensions which will be blocked by MMC security policy anyway
+        "--ignore-certificate-errors",
+        "enable-automation",
+        "--disable-dev-shm-usage",
+        "--disable-browser-side-navigation",
+        "--no-sandbox",
+        "--headless",
+        "--remote-debugging-port=9222",
+        "--disable-extensions",
+      ],
+    },
   },
   workers: 2,
   projects: [
